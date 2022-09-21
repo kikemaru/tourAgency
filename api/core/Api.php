9126aec5ace2b5ac05ce005213d4cf7be78086e4
Roman Romanov <kikemaru@mail.ru>
2022-09-21 13:52:57 +0300

    Update (add new method for api)

--- api/core/Api.php
+++ api/core/Api.php
@@ -6,11 +6,61 @@ use PDO;
 class Api
 {
 
-    public $db;
+    private $db;
 
     public function __construct($config = array())
     {
         $this->db = new PDO('mysql:host='.$config['dbhost'].';dbname='.$config['dbname'].';', $config['dbuser'], $config['dbpass']);
     }
+
+
+    public function getUser($token, $params = array())
+    {
+        $checkToken = $this->checkToken($token);
+        if ($checkToken)
+        {
+            return $this->createJson(array('data' => array('data' => '')));
+        } else {
+            return $this->createJson(array('error' => 400, 'desc' => 'Error api token'));
+        }
+    }
+
+
+    public function getTour($token, $params = array())
+    {
+
+    }
+
+
+    public function setUser($token, $params = array())
+    {
+
+    }
+
+
+    public function setTour($token, $params = array())
+    {
+
+    }
+
+
+    private function checkToken($token)
+    {
+        $token = htmlspecialchars(stripslashes(urldecode($token)));
+        $db = $this->db->query("SELECT * FROM token WHERE token = '$token'")->fetch();
+        if ($db['id'] != 0)
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    private function createJson($params = array())
+    {
+        return json_encode($params);
+    }
+
 
 }
